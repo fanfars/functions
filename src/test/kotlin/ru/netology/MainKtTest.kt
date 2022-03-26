ac976461509b103e5d0b68b7b27e857ddd32b0d2
fanfars <fanissd@gmail.com>
2022-03-26 12:53:13 +0400

    Test broken

--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -9,7 +9,7 @@ class MainKtTest {
     @Test
     fun doubleFormatTypeRubOnly() {
         val valueDouble = 10.0
-        val expectedFormat = "10 руб."
+        val expectedFormat = "11 руб."
 
         val actualFormat = doubleFormat(valueDouble)
 
